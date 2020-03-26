3f42b93d7e9cad8c988f8c1942d3ef7765c77c96
patrick-gordon <patrickgordon42@yahoo.com>
2020-03-26 14:10:58 -0500

    headers

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const strainsRouter = require('./strains/strains-router');
 const helmet = require('helmet');
 
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
